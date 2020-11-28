055ca642dc2a5c7a55befb7f26c8c1b13abc9e8c
Andrew Dolgert <adolgert@uw.edu>
2020-11-28 11:59:17 -0800

    more testing of the one parameter combinations

--- test/test_combinations.jl
+++ test/test_combinations.jl
@@ -74,3 +74,17 @@ minimal = UnitTestDesign.one_parameter_combinations([2, 3], 1)
 
 paired = UnitTestDesign.one_parameter_combinations([2, 3], 2)
 @test paired == [1 1; 2 1; 1 2; 2 2; 1 3; 2 3]
+
+singled = UnitTestDesign.one_parameter_combinations([2, 2, 3], 2)
+@test singled == [
+    1 0 1; 2 0 1; 0 1 1; 0 2 1;
+    1 0 2; 2 0 2; 0 1 2; 0 2 2;
+    1 0 3; 2 0 3; 0 1 3; 0 2 3
+    ]
+
+again = UnitTestDesign.one_parameter_combinations([2, 2, 3], 3)
+@test again == [
+    1 1 1; 1 2 1; 2 1 1; 2 2 1;
+    1 1 2; 1 2 2; 2 1 2; 2 2 2;
+    1 1 3; 1 2 3; 2 1 3; 2 2 3
+    ]
