6c7f4c9271a3695d00422492f2ce5002a71ad0b9
Andrew Dolgert <adolgert@uw.edu>
2020-11-29 08:07:57 -0800

    flipped the coverage set

--- test/test_coverage_set.jl
+++ test/test_coverage_set.jl
@@ -4,12 +4,12 @@ using UnitTestDesign
 
 ### coverage_by_parameter
 cbp_cases = [
-    [[0 0 0; 0 0 0; 0 1 0], 3, [0, 1, 0]],
-    [[0 0 0; 0 0 0; 0 1 0], 2, [0, 0, 0]],
-    [[0 0 7; 0 0 4; 0 1 0], 3, [0, 1, 2]]
+    [[0 0 0; 0 0 0; 0 1 0]', 3, [0, 1, 0]],
+    [[0 0 0; 0 0 0; 0 1 0]', 2, [0, 0, 0]],
+    [[0 0 7; 0 0 4; 0 1 0]', 3, [0, 1, 2]]
 ]
 for cbp_case in cbp_cases
-    mc = UnitTestDesign.MatrixCoverage(cbp_case[1], cbp_case[2], [2, 2, 7])
+    mc = UnitTestDesign.MatrixCoverage(collect(cbp_case[1]), cbp_case[2], [2, 2, 7])
     res = UnitTestDesign.coverage_by_parameter(mc)
     @test res == cbp_case[3]
 end
@@ -24,22 +24,22 @@ mc_opcm = UnitTestDesign.one_parameter_combinations_matrix([2, 2, 3], 2)
 # (coverage matrix, remaining uncovered, arity,
 #  parameter, coverage of that parameter)
 cbv_cases = [
-    [[0 0 0; 0 0 0; 0 0 0], 3, [2,2,2], 1, [0, 0]],
-    [[0 0 0; 0 0 0; 0 0 0], 3, [2,2,2], 2, [0, 0]],
-    [[0 0 0; 0 0 0; 0 0 0], 3, [2,3,2], 2, [0, 0, 0]], # sees arity of choice
-    [[0 1 0; 0 1 0; 0 0 0], 3, [2,3,2], 2, [2, 0, 0]], # does multiple rows
-    [[0 1 0; 0 1 0; 0 3 0], 3, [2,3,2], 2, [2, 0, 1]], # does multiple columns
-    [[0 1 0; 0 1 0; 0 3 0], 2, [2,3,2], 2, [2, 0, 0]]  # excludes extra rows
+    [[0 0 0; 0 0 0; 0 0 0]', 3, [2,2,2], 1, [0, 0]],
+    [[0 0 0; 0 0 0; 0 0 0]', 3, [2,2,2], 2, [0, 0]],
+    [[0 0 0; 0 0 0; 0 0 0]', 3, [2,3,2], 2, [0, 0, 0]], # sees arity of choice
+    [[0 1 0; 0 1 0; 0 0 0]', 3, [2,3,2], 2, [2, 0, 0]], # does multiple rows
+    [[0 1 0; 0 1 0; 0 3 0]', 3, [2,3,2], 2, [2, 0, 1]], # does multiple columns
+    [[0 1 0; 0 1 0; 0 3 0]', 2, [2,3,2], 2, [2, 0, 0]]  # excludes extra rows
 ]
 for cbv_case in cbv_cases
-    mc = UnitTestDesign.MatrixCoverage(cbv_case[1], cbv_case[2], cbv_case[3])
+    mc = UnitTestDesign.MatrixCoverage(collect(cbv_case[1]), cbv_case[2], cbv_case[3])
     res = UnitTestDesign.coverage_by_value(mc, cbv_case[4])
     @test res == cbv_case[5]
 end
 
 
 ### most_matches_existing(allc, row_cnt, arity, existing, param_idx)
-mmm_allc = [1 1 1 0 0; 1 2 1 0 0; 0 0 1 2 2]
+mmm_allc = [1 1 1 0 0; 1 2 1 0 0; 0 0 1 2 2]'
 mmm_arity = [2, 2, 2, 2, 2]
 mmm_cases = [
     [[1, 0, 0, 0, 0], 2, [1, 1]],
@@ -55,7 +55,7 @@ mmm_cases = [
     [[0, 0, 1, 0, 2], 4, [0, 1]]
 ]
 for mmm_case in mmm_cases
-    mc = UnitTestDesign.MatrixCoverage(mmm_allc, 3, mmm_arity)
+    mc = UnitTestDesign.MatrixCoverage(collect(mmm_allc), 3, mmm_arity)
     res = UnitTestDesign.most_matches_existing(mc, mmm_case[1], mmm_case[2])
     @test res == mmm_case[3]
 end
@@ -68,15 +68,15 @@ arity4 = [3, 2, 2]
 allc4 = [
     1 0 1; 2 0 1; 3 0 1; 0 1 1; 0 2 1;
     1 0 2; 2 0 2; 3 0 2; 0 1 2; 0 2 2
-]
-mc4 = UnitTestDesign.MatrixCoverage(allc4, size(allc4, 1), arity4)
+]'
+mc4 = UnitTestDesign.MatrixCoverage(collect(allc4), size(allc4, 2), arity4)
 mm4 = UnitTestDesign.matches_from_missing(mc4, wider[1, :], 3)
 @test mm4 == [2, 2]
 
 ### first_match_for_parameter(mc, param_idx)
 arity = [3, 2, 2, 2]
-mat = [0 1 2 0; 2 2 1 0]
-mcfm = UnitTestDesign.MatrixCoverage(mat, size(mat, 1), arity)
+mat = [0 1 2 0; 2 2 1 0]'
+mcfm = UnitTestDesign.MatrixCoverage(collect(mat), size(mat, 2), arity)
 first = UnitTestDesign.first_match_for_parameter(mcfm, 3)
 @test first == [0, 1, 2, 0]
 blank = UnitTestDesign.first_match_for_parameter(mcfm, 1)
@@ -93,8 +93,8 @@ mat = [
     0 2 0;
     0 0 3;
     0 0 4
-]
-mc = UnitTestDesign.MatrixCoverage(mat, size(mat, 1), arity)
+]'
+mc = UnitTestDesign.MatrixCoverage(collect(mat), size(mat, 2), arity)
 res1 = UnitTestDesign.fill_consistent_matches(mc, [0, 2, 0])
 @test res1 == [1, 2, 3]
 
@@ -105,8 +105,8 @@ mat2 = [
     0 2 0;
     1 0 4;
     0 0 3
-]
-mc2 = UnitTestDesign.MatrixCoverage(mat2, size(mat2, 1), arity)
+]'
+mc2 = UnitTestDesign.MatrixCoverage(collect(mat2), size(mat2, 2), arity)
 res2 = UnitTestDesign.fill_consistent_matches(mc2, [0, 2, 0])
 @test res2 == [1, 2, 4]
 
@@ -117,8 +117,8 @@ mat3 = [
     0 2 0;
     0 0 3;
     1 0 4
-]
-mc3 = UnitTestDesign.MatrixCoverage(mat3, size(mat3, 1), arity)
+]'
+mc3 = UnitTestDesign.MatrixCoverage(collect(mat3), size(mat3, 2), arity)
 res3 = UnitTestDesign.fill_consistent_matches(mc3, [0, 2, 0])
 @test res3 == [1, 2, 3]
 
@@ -127,13 +127,13 @@ res3 = UnitTestDesign.fill_consistent_matches(mc3, [0, 2, 0])
 # when a new test covers values. It expects a particular
 # reordering due to swapping. Very much an implementation test.
 ac_cases = [
-    [[1 1 0; 1 2 0; 1 0 1; 1 0 2], 4, 2, [1, 1, 1], 2, [1 0 2; 1 2 0; 1 1 0; 1 0 1]],
-    [[1 1 0; 1 2 0; 1 0 1; 1 0 2], 3, 2, [1, 1, 1], 1, [1 2 0; 1 1 0; 1 0 1; 1 0 2]],
-    [[1 0 2; 0 2 2; 0 1 2], 1, 2, [1, 0, 2], 0, [1 0 2; 0 2 2; 0 1 2]],
-    [[1 0 2; 0 2 2; 0 1 2], 1, 2, [1, 0, 1], 1, [1 0 2; 0 2 2; 0 1 2]]
+    [[1 1 0; 1 2 0; 1 0 1; 1 0 2]', 4, 2, [1, 1, 1], 2, [1 0 2; 1 2 0; 1 1 0; 1 0 1]'],
+    [[1 1 0; 1 2 0; 1 0 1; 1 0 2]', 3, 2, [1, 1, 1], 1, [1 2 0; 1 1 0; 1 0 1; 1 0 2]'],
+    [[1 0 2; 0 2 2; 0 1 2]', 1, 2, [1, 0, 2], 0, [1 0 2; 0 2 2; 0 1 2]'],
+    [[1 0 2; 0 2 2; 0 1 2]', 1, 2, [1, 0, 1], 1, [1 0 2; 0 2 2; 0 1 2]']
 ]
 for ac_case in ac_cases
-    mcac = UnitTestDesign.MatrixCoverage(ac_case[1], ac_case[2], [2, 3, 2])
+    mcac = UnitTestDesign.MatrixCoverage(collect(ac_case[1]), ac_case[2], [2, 3, 2])
     UnitTestDesign.add_coverage!(mcac, ac_case[4])
     @test mcac.remain == ac_case[5]
     @test mcac.allc == ac_case[6]
@@ -142,14 +142,14 @@ end
 
 ### match_score
 ms_cases = [
-    [[1 1 0; 1 2 0; 0 1 3], 3, 2, [4,4,4], 0],
-    [[1 1 0; 1 2 0; 0 2 1; 0 1 3], 4, 2, [1,1,1], 1],
-    [[1 1 0; 1 2 0; 0 2 1; 0 1 3], 4, 2, [1,2,1], 2],
-    [[1 1 0; 1 2 0; 0 2 1; 0 1 3], 4, 2, [1,1,3], 2],
-    [[1 1 0; 1 2 0; 0 2 1; 0 1 3], 3, 2, [1,1,3], 1]
+    [[1 1 0; 1 2 0; 0 1 3]', 3, 2, [4,4,4], 0],
+    [[1 1 0; 1 2 0; 0 2 1; 0 1 3]', 4, 2, [1,1,1], 1],
+    [[1 1 0; 1 2 0; 0 2 1; 0 1 3]', 4, 2, [1,2,1], 2],
+    [[1 1 0; 1 2 0; 0 2 1; 0 1 3]', 4, 2, [1,1,3], 2],
+    [[1 1 0; 1 2 0; 0 2 1; 0 1 3]', 3, 2, [1,1,3], 1]
 ]
 for ms_case in ms_cases
-    mc_ms = UnitTestDesign.MatrixCoverage(ms_case[1], ms_case[2], [4, 4, 4])
+    mc_ms = UnitTestDesign.MatrixCoverage(collect(ms_case[1]), ms_case[2], [4, 4, 4])
     cnt = UnitTestDesign.match_score(mc_ms, ms_case[4])
     @test cnt == ms_case[5]
 end
@@ -157,24 +157,18 @@ end
 
 ## remove_combinations
 rc_cases = [
-    [[1 1 1; 1 1 2; 1 2 1; 1 2 2], (x -> x[2] == 1 && x[3] == 1), [1 1 2; 1 2 1; 1 2 2]],
-    [[1 1 1; 1 1 2; 1 2 1; 1 2 2], (x -> x[2] == 1), [1 2 1; 1 2 2]],
-    [[1 1 1; 1 1 2; 1 2 1; 1 2 2], (x -> x[1] == 1 && x[3] == 1), [1 1 2; 1 2 2]],
-    [[1 1 1; 1 1 2; 1 2 1; 1 2 2], (x -> x[2] < x[3]), [1 1 1; 1 2 1; 1 2 2]]
+    [[1 1 1; 1 1 2; 1 2 1; 1 2 2]', (x -> x[2] == 1 && x[3] == 1), [1 1 2; 1 2 1; 1 2 2]'],
+    [[1 1 1; 1 1 2; 1 2 1; 1 2 2]', (x -> x[2] == 1), [1 2 1; 1 2 2]'],
+    [[1 1 1; 1 1 2; 1 2 1; 1 2 2]', (x -> x[1] == 1 && x[3] == 1), [1 1 2; 1 2 2]'],
+    [[1 1 1; 1 1 2; 1 2 1; 1 2 2]', (x -> x[2] < x[3]), [1 1 1; 1 2 1; 1 2 2]']
 ]
 for rc_case in rc_cases
-    mc_rc = UnitTestDesign.MatrixCoverage(rc_case[1], 4, [2, 3, 2])
+    mc_rc = UnitTestDesign.MatrixCoverage(collect(rc_case[1]), 4, [2, 3, 2])
     UnitTestDesign.remove_combinations!(mc_rc, rc_case[2])
     @test mc_rc.allc == rc_case[3]
 end
 
 
-
-
-
-
-
-
 ## multi_way_coverage
-mwc = UnitTestDesign.multi_way_coverage([2,3,4,2,2,3], Dict(3 => [[1,4,4,5]]), 2)
-@test maximum(sum(mwc .!= 0, dims = 2)) == 3
+mwc = UnitTestDesign.multi_way_coverage([2,3,4,2,2,3], Dict(3 => [[1,3,4,5]]), 2)
+@test maximum(sum(mwc .!= 0, dims = 1)) == 3
