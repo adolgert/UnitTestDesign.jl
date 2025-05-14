2a08bf828c5f0d5919c1f3c8c450be0b509a4358
Drew Dolgert <adolgert@uw.edu>
2025-05-14 07:27:23 -0400

    Moving tests around so that the script is tested

--- scripts/tuples.jl
+++ scripts/tuples.jl
@@ -1,10 +1,13 @@
+using TestItemRunner
+
+module CLITuples
+
 using Pkg
 using TOML
 
 using ArgParse
 using CSV
 using UnitTestDesign
-
 
 # This function has the same signature as the all_tuples function but encodes
 # the parameters in a dictionary for a TOML file.
@@ -173,5 +176,106 @@ function julia_main()::Cint
     end
     return 0
 end
+export write_testcases, julia_main, parse_commandline, config_from_io, convert_to_dict_all_tuples
+end
 
+using .CLITuples
 julia_main()
+
+
+@testitem "Write and read TOML" begin
+    using TOML
+    using CLITuples
+    toml_contents = convert_to_dict_all_tuples(
+        [1, 2, 3],
+        ["a", "b", "c"],
+        [1, 5, 7],
+        [7, 9, 11],
+        [1, 2, 3];
+        n_way = 2,
+        engine = IPOG(),
+        disallow = nothing,
+        seeds = [[3, "a", 5, 7, 1], [1, "a", 7, 11, 3]],
+        wayness = Dict(3 => [[1, 3, 4, 5]]),
+        Counter = Int8,
+    )
+    io = IOBuffer()
+    TOML.print(io, toml_contents, sorted=true)
+    io_rewound = seek(io, 0)
+    parameters, config = config_from_io(io_rewound)
+    @test config[:n_way] == 2
+    @test config[:engine] == IPOG()
+    @test :disallow ∉ keys(config)
+    @test config[:wayness] == Dict(3 => [[1, 3, 4, 5]])
+    @test config[:Counter] == Int8
+    @test parameters == Any[
+        [1, 2, 3],
+        ["a", "b", "c"],
+        [1, 5, 7],
+        [7, 9, 11],
+        [1, 2, 3],
+    ]
+    @test config[:seeds] == Any[[3, "a", 5, 7, 1], [1, "a", 7, 11, 3]]
+end
+
+
+@testitem "commandline parser makes sense" begin
+    using .CLITuples
+    parsed = parse_commandline(split("""
+        --n-way 2 input.toml
+        """))
+    @test parsed["n-way"] == 2
+    @test parsed["input"] == "input.toml"
+    @test parsed["output"] === nothing
+
+    parsed = parse_commandline(split("""
+        festoon.toml calibrate.csv
+        """))
+    @test parsed["n-way"] == 0
+    @test parsed["input"] == "festoon.toml"
+    @test parsed["output"] == "calibrate.csv"
+end
+
+
+@testitem "end-to-end parsing" begin
+    using TOML
+    using .CLITuples
+
+    toml_contents = convert_to_dict_all_tuples(
+        [1, 2, 3],
+        ["a", "b", "c"],
+        [1, 5, 7],
+        [7, 9, 11],
+        [1, 2, 3];
+        n_way = 2,
+        engine = IPOG(),
+        disallow = nothing,
+        seeds = [[3, "a", 5, 7, 1], [1, "a", 7, 11, 3]],
+        wayness = Dict(3 => [[1, 3, 4, 5]]),
+        Counter = Int8,
+    )
+    io = IOBuffer()
+    TOML.print(io, toml_contents, sorted=true)
+    io_rewound = seek(io, 0)
+    parameters, kwargs = config_from_io(io_rewound)
+    compare_contents = convert_to_dict_all_tuples(parameters...; kwargs...)
+
+    @test Set(keys(toml_contents)) == Set(keys(compare_contents))
+end
+
+
+@testitem "write_testcases writes and reads correctly" begin
+    using CSV
+    using .CLITuples
+    testcases = [[i, i+1, string(Char('a'+i-1))] for i in 1:10]
+    io = IOBuffer()
+    write_testcases(io, testcases)
+    seek(io, 0)
+    tbl = CSV.File(io; header=false)
+    # Convert CSV.File rows to lists for comparison
+    read_cases = [collect(row) for row in tbl]
+    # Convert all elements to string for comparison, since CSV.File may infer types
+    input_cases_str = [[string(x) for x in row] for row in testcases]
+    read_cases_str = [[string(x) for x in row] for row in read_cases]
+    @test input_cases_str == read_cases_str
+end
--- src/UnitTestDesign.jl
+++ src/UnitTestDesign.jl
@@ -24,6 +24,5 @@ include("parameter_order.jl")
 include("full_factorial.jl")
 include("excursions.jl")
 include("factorial_interface.jl")
-include("commandline.jl")
 
 end
